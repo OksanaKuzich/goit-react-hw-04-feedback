b1cd6a08b9c1797ed8d438e9a02ac80b41a65e89
OksanaKuzich <komoks255@gmail.com>
2022-11-09 16:44:22 +0200

    changed to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,60 @@
-import { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
+import { useState } from 'react';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleIncrementGood = e => {
+    const currentName = e.target.name;
+    
+    switch (currentName) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        return
+    }
   };
 
-  handleIncrement = e => {
-    this.setState(prevState => ({
-      [e.target.name]: prevState[e.target.name] + 1,
-    }));
-  };
-
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
+  function countTotalFeedback() {
+    return good + neutral + bad;
   }
 
-  countPositiveFeedbackPercentage() {
-    let total = this.countTotalFeedback();
-    return ((this.state.good / total) * 100).toFixed(0);
+  function countPositiveFeedbackPercentage() {
+    let total = countTotalFeedback();
+    return ((good / total) * 100).toFixed(0);
   }
 
-  render() {
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleIncrement}
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={Object.keys({good, neutral, bad})} onLeaveFeedback={handleIncrementGood} />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() === 0 ? (
+          <Notification message="There is no feedback"></Notification>
+        ) : (
+          <Statistics
+            title=""
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() === 0 ? (
-            <Notification message="There is no feedback"></Notification>
-          ) : (
-            <Statistics
-              title=""
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          )}
-        </Section>
-      </>
-    );
-  }
-}
+        )}
+      </Section>
+    </>
+  );
+};
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -2,9 +2,7 @@ import PropTypes from 'prop-types';
 
 export const Notification = ({ message }) => {
   return (
-    <>
       <p>{message}</p>
-    </>
   );
 };
 
